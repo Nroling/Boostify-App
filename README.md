5fee14b399c098da1d7d040d11701b6cfa1f1db0
Nroling <rolingnick@gmail.com>
2025-03-10 17:11:59 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Boostify-App
-Live song requests, tipping, and boosting for DJs &amp; performers.”
+# Boostify - Live Song Requests & Tipping App
+Boostify lets users request songs, tip performers, and boost songs to the top of the queue.
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BoostifyApp from "./BoostifyApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BoostifyApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
